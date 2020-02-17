a7a2d054174ec8b56487d42612f9f6e9f7493c9f
M Groesbeck <m.groesbeck@gmail.com>
2020-02-17 14:15:45 -0800

    Able to increment quarter and reset game. Formatting and start/stop button implemented for timer, so countdown could be implemented later. All displayed numbers come from variables, not hard-coded.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, {useState} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 import TopRow from "./TopRow";
+import Buttons from "./Buttons";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
@@ -12,6 +13,8 @@ function App() {
   const [toGo, setToGo] = useState(10);
   const [ballOn, setBallOn] = useState(50);
   const [quarter, setQuarter] = useState(1);
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(15);
   const bottomRowData = {
     down: down,
     toGo: toGo,
@@ -20,26 +23,40 @@ function App() {
   }
   const topRowData = {
     homeScore: homeScore,
-    awayScore: awayScore
+    awayScore: awayScore,
+    minutes: minutes,
+    seconds: seconds
   }
-  
+  const buttonsData = {
+    handler: handler,
+    newGame: newGame,
+    incQuarter: incQuarter
+  }
+
+function handler(team,points) {
+    team === 'home' ? setHomeScore(homeScore + points) : setAwayScore(awayScore + points);
+  }
+
+  function newGame(){
+    setHomeScore(0);
+    setAwayScore(0);
+    setDown(1);
+    setToGo(10);
+    setQuarter(1);
+    setMinutes(15);
+    setSeconds(0);
+    setBallOn(50);
+  }
+
+  function incQuarter() {quarter < 4 ? setQuarter(quarter+1) : newGame();};
+
   return (
     <div className="container">
       <section className="scoreboard">
         <TopRow dat={topRowData} />
         <BottomRow dat={bottomRowData} />
       </section>
-      <section className="buttons">
-        <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick = {() => setHomeScore(homeScore + 7)}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick = {() => setHomeScore(homeScore + 3)}>Home Field Goal</button>
-        </div>
-        <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick = {() => setAwayScore(awayScore + 7)}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick = {() => setAwayScore(awayScore + 3)}>Away Field Goal</button>
-        </div>
-      </section>
+      <Buttons dat={buttonsData}/>
     </div>
   );
 }
